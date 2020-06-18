7195810101d8725cf6834bb6a76d3679ac5782a7
svkrdj <svkrclg@gmai.com>
2020-06-18 20:27:56 +0530

    Databases added

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const app = express();
 var path = require('path');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const Firestore = require('@google-cloud/firestore');
+
+const db = new Firestore({
+  projectId: 'theta-outrider-280713',
+  keyFilename: './My-website-1c2f5e0e193e.json',
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/inc', express.static(__dirname + '/inc'));
 app.use('/js', express.static(__dirname + '/js'));
+
+
+
+
 app.listen(port,function(){
     console.log("running on port ", port);
 });
@@ -23,9 +34,21 @@ app.post("/message_me", (req, res)=>{
     var email = req.body.email;
     var message = req.body.message;
     console.log(name, email, message);
-    res.sendStatus(200);
-    res.end();
-    //insert in database
-    
-
+    var date = new Date();
+    var timestamp = date.getTime();
+    console.log(timestamp);
+    let docRef = db.collection('my-website').doc(timestamp.toString());
+    docRef.set({
+        "name": name ,
+        "email": email,
+        "message": message
+      }).then(r=>{
+          console.log("Doucment written", r);
+          res.sendStatus(200)
+          return;
+      }).catch(e=>{
+          console.log(e);
+          res.sendStatus(500);
+          return;
+      });
 })
